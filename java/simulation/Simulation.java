0e8b8c9fc1529edc0a63bf44909383240e2df9b9
Sergio Smirnoff <ssmirnoff@itba.edu.ar>
2025-09-02 19:11:39 -0300

    small change

--- java/simulation/Simulation.java
+++ java/simulation/Simulation.java
@@ -2,9 +2,8 @@ package EventDrivenMolecularDynamic.java.simulation;
 
 public class Simulation {
 
-    private final double widthFirstBox = 0.09;
     private final double heightFirstBox = 0.09;
-    private final double widthSecondBox = 0.09;
+    private final double width = 0.09;
     private double heightSecondBox = 0.09; // this is a variable
     private final double ballMass = 1; // in kg
     private final double ballRadius = 0.0015; // meters
@@ -16,18 +15,17 @@ public class Simulation {
         this.heightSecondBox = heightSecondBox;
     }
 
-    public void runSimulation() {
-        // Simulation logic goes here
-
-
-
-
-    }
-
     private int calculateNextCollision() {
         // Collision detection and response logic for the second box goes here
         return 0;
     }
+
+
+    /*
+     * Saves the current state of the simulation to a file.
+     * Save in csv format
+     * time;positionX;positionY;velocityX;velocityY
+     */
 
     private boolean saveSimulationState(String filePath) {
         // Logic to save the simulation state to a file
@@ -51,10 +49,16 @@ public class Simulation {
         return 0;
     }
 
-    private double calculateImpulse(){
+    private double calculateCollisionImpulse(){
         return 0;
     }
 
+    public void runSimulation() {
+        // Simulation logic goes here
 
+
+
+
+    }
 
 }
