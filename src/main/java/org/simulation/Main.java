c39b3ae5fa84819dc75206f11f0a9488c3d8dd72
Patricklt2 <108370425+Patricklt2@users.noreply.github.com>
2025-09-12 11:45:38 -0300

    fix

--- src/main/java/org/simulation/Main.java
+++ src/main/java/org/simulation/Main.java
@@ -9,11 +9,11 @@ public class Main {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
 
-        double heightSecondBox = 0.07;
-        int particlesCount = 300;
+        double heightSecondBox = 0.03;
+        int particlesCount = 200;
 
         Simulation simulation = new Simulation(heightSecondBox, particlesCount);
-        simulation.runSimulation(5000, String.format("initial_state_%.2f.csv", heightSecondBox));
+        simulation.runSimulation(2000, String.format("initial_state_%.2f.csv", heightSecondBox));
     }
 
 }
